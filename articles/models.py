e48d35bf906c0739aaea0861f4d6b65db4e449de
irfunny70 <tiredlah@gmail.com>
2024-01-12 07:52:31 +0800

    added a slug fieldfrom ep37

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True,null=True)
     content = models.TextField()
+    #last created
+    timestamp = models.DateTimeField(auto_now_add=True)
+    #when it was last updated
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self,*args,**kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
